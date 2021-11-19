bc9a8fb6754b2d18b8961d19be053d80bcfa6bea
Jefferson Lam <jefferson.lam@polymtl.ca>
2021-11-19 16:45:20 -0500

    Fixed wrong merge conflict

--- setup.py
+++ setup.py
@@ -93,8 +93,7 @@ skbuild.setup(
     package_dir={"": "python/ecole/src"},
     package_data={"ecole": ["py.typed"]},
     cmake_languages=["CXX"],
-    cmake_minimum_required_version="3.14",
-    cmake_install_dir="python/src",
+    cmake_install_dir="python/ecole/src/ecole",  # Must match package_dir layout
     # FIXME No way to pass cmake argument to scikit-build through pip (for now)
     # https://github.com/scikit-build/scikit-build/issues/479
     # So we read them from an environment variable
