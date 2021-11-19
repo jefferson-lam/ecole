f84e65de8f55edc8004fee7bf20530c395887a90
Jefferson Lam <jefferson.lam@polymtl.ca>
2021-11-19 16:26:50 -0500

    Add the compilation step for the cutting component of the library

--- libecole/include/ecole/dynamics/cutting.hpp
+++ libecole/include/ecole/dynamics/cutting.hpp
@@ -7,17 +7,18 @@
 #include <xtensor/xtensor_forward.hpp>
 
 #include "ecole/dynamics/dynamics.hpp"
+#include "ecole/export.hpp"
 
 namespace ecole::dynamics {
 class CuttingDynamics : public EnvironmentDynamics<std::size_t, std::optional<xt::xtensor<std::size_t, 1>>> {
 public:
 	using ActionSet = std::optional<xt::xtensor<std::size_t, 1>>;
 
-	CuttingDynamics();
+	ECOLE_EXPORT CuttingDynamics();
 
-	std::tuple<bool, ActionSet> reset_dynamics(scip::Model& model) override;
+	ECOLE_EXPORT std::tuple<bool, ActionSet> reset_dynamics(scip::Model& model) override;
 
-	std::tuple<bool, ActionSet> step_dynamics(scip::Model& model, std::size_t const& var_idx) override;
+	ECOLE_EXPORT std::tuple<bool, ActionSet> step_dynamics(scip::Model& model, std::size_t const& var_idx) override;
 };
 
 }  // namespace ecole::dynamics
